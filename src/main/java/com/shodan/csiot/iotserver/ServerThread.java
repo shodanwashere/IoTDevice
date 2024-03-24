be8bedc4ad294c03a032b29317beebf0e9ec6e19
Nuno Dias <shodantltwb@proton.me>
2024-03-24 18:44:46 +0000

    Properly implemented initial stage of server thread

--- src/main/java/com/shodan/csiot/iotserver/ServerThread.java
+++ src/main/java/com/shodan/csiot/iotserver/ServerThread.java
@@ -1,20 +1,103 @@
 package com.shodan.csiot.iotserver;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.File;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.io.IOException;
+import java.net.Socket;
 
 public class ServerThread extends Thread {
-  private Logger logger = null;
+  private Logger logger;
 
-  private BufferedReader passwdFileBufferedReader;
-  private BufferedWriter passwdFileBufferedWriter;
+  private File passwd;
+  private File domains;
 
-  public void set(BufferedReader passwdFileBufferedReader, BufferedWriter passwdFileBufferedWriter){
-    this.passwdFileBufferedReader = passwdFileBufferedReader;
-    this.passwdFileBufferedWriter = passwdFileBufferedWriter;
+  private Map<String,String> usersAndPasswords = new HashMap<>();
+  private Map<String,List<String>> domainUserPermissions = new HashMap<>();
+  private Map<String,List<String>> domainDeviceList = new HashMap<>();
+
+  private Socket cliSocket;
+
+  private volatile boolean shutdownInitiated = false;
+
+  public void stopExecution(){
+    shutdownInitiated = true;
+  }
+
+  public void set(File passwd, File domains, Socket cliSocket) throws Exception {
+    // set up socket, file descriptors and shutdown flag
+    this.cliSocket = cliSocket;
+    this.passwd = passwd;
+    this.domains = domains;
+
+    // open the passwd file and copy contents to RAM
+    synchronized(passwd){
+      FileReader pfr = new FileReader(passwd);
+      BufferedReader pbfr = new BufferedReader(pfr);
+
+      String line;
+      while((line = pbfr.readLine()) != null){
+        String[] pair = line.split(":");
+	usersAndPasswords.put(new String(pair[0]), new String(pair[1])); // add passwd file entries to hashmap
+      }
+
+      pbfr.close();
+      pfr.close();
+    }
+
+    // open the domains file and copy its contents to RAM
+    synchronized(domains){
+      FileReader dfr = new FileReader(domains);
+      BufferedReader dbfr = new BufferedReader(dfr);
+
+      String line;
+      while((line = dbfr.readLine()) != null) {
+        String[] tuple = line.split(":", Integer.MAX_VALUE);
+	String domain = new String(tuple[0]);
+	String users = new String(tuple[1]);
+	String devices = new String(tuple[2]);
+	List<String> userList = new ArrayList<>();
+	List<String> deviceList = new ArrayList<>();
+
+	if(!users.equals("")){
+	  for(String u: users.split(",")){
+	    userList.add(u);
+	  }
+	}
+
+	if(!devices.equals("")){
+	  for(String d: devices.split(",")){
+	    deviceList.add(d);
+	  }
+	}
+
+	domainUserPermissions.put(domain, userList);
+	domainDeviceList.put(domain, deviceList);
+      }
+
+      dbfr.close();
+      dfr.close();
+    }
+
   }
 
   public void run() {
-    logger = new Logger(Thread.currentThread().getName()); // getting thread name on new thread. doing this on the constructor will get the name of the main thread
-    logger.log("I'm a thread!");
+    Thread.currentThread().setName(cliSocket.getRemoteSocketAddress().toString());
+    logger = new Logger(Thread.currentThread().getName());
+    while(!shutdownInitiated) {
+    }
+    
+    try {
+      logger.log("Connection closed! Closing socket!");
+      cliSocket.close();
+    } catch (IOException e) {
+      logger.logErr("Socket could not be closed!");
+    }
+    return;
   }
 }
