5932bc224d7a9dc37331a7cca8fc73e9ba164413
Nuno Dias <nuno.dias@celfocus.com>
2024-03-25 14:54:36 +0000

    Fixed socket connection

--- src/main/java/com/shodan/csiot/IoTDevice.java
+++ src/main/java/com/shodan/csiot/IoTDevice.java
@@ -13,6 +13,10 @@ import java.io.IOException;
 public class IoTDevice {
   public static Socket clientSocket;  
   public static Scanner input = new Scanner(System.in);
+
+  /**
+   * Displays a help message to the CLI
+   */
   public static void help(){
     System.out.println("Available commands:");
     System.out.println("- CREATE <dm>        - Create a new domain named 'dm' under your ownership");
@@ -26,21 +30,35 @@ public class IoTDevice {
     System.out.println("- EXIT               - Terminate connection");
   }
 
-  public static void commandLineInterface(){
+  /**
+   * Method that deals with the entire command line interface
+   * 
+   * @param in - Client socket input stream
+   * @param in - Client socket output stream
+   */
+  public static void commandLineInterface(ObjectInputStream in, ObjectOutputStream out){
     help();
     while(true){
       System.out.printf("> ");
       String command = input.nextLine();
       String keyword = new String(command.split(" ")[0]);
       switch(keyword){
-        case "CREATE": createCommand(command); break;
-        case "EXIT": exitCommand(); return;
+        case "CREATE": createCommand(command, in, out); break;
+        case "HELP": help(); break;
+        case "EXIT": exitCommand(in, out); return;
         default: System.out.println("Not implemented yet"); break;
       }
     }
   }
 
-  public static void createCommand(String command){
+  /**
+   * Implements the CREATE command on the client side
+   * 
+   * @param command - String that contains the CREATE keyword and the @code{dm} parameter
+   * @param in - Client socket input stream
+   * @param in - Client socket output stream
+   */
+  public static void createCommand(String command, ObjectInputStream in, ObjectOutputStream out){
     String[] splitCommand = command.split(" ");
 
     if(splitCommand.length != 2){
@@ -50,8 +68,6 @@ public class IoTDevice {
 
       String dm = new String(splitCommand[1]);
       try { 
-        ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
-        ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
 
         out.writeObject(Command.CREATE);
         out.writeObject(dm);
@@ -60,9 +76,6 @@ public class IoTDevice {
           case OK : System.out.println("New domain "+dm+" created"); break;
           case NOK : System.err.println("Error: server could not create domain"); break;
         }
-
-        out.close();
-        in.close();
       } catch (IOException e) {
         System.err.println("Error: failed to communicate with server");
       } catch (ClassNotFoundException e) {
@@ -72,17 +85,25 @@ public class IoTDevice {
     return;
   }
 
-  public static void exitCommand(){
+  /**
+   * Implements an EXIT command instead of CTRL+C
+   * 
+   * @param in - Client socket input stream
+   * @param out - Client socket output stream
+   */
+  public static void exitCommand(ObjectInputStream in, ObjectOutputStream out){
     try {
-      ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
       out.writeObject(Command.EOF);
-      out.close();
+      out.flush();
     } catch (IOException e) {
       // do nothing, just exit
     }
     return;
   }
 
+  /**
+   * main method
+   */
   public static void main(String[] args){
     // process args
     if(args.length < 3){
@@ -107,21 +128,30 @@ public class IoTDevice {
         port = Integer.parseInt(addressAndPort[1]);
       }
       clientSocket = new Socket(address, port);
-    } catch (Exception e) {
-      System.err.println(e.getMessage());
-      System.exit(1);
-    }
+      // System.out.println("Passed all arg checks");
 
-    System.out.println("Passed all arg checks");
+      // get streams from socket
+      ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+      ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
 
-    commandLineInterface();
+      // initialize CLI
+      commandLineInterface(in, out);
 
-    try {
+      // if the code reaches this point, that means that the client chose to EXIT
+      // time to shut everything down.
       System.out.println("Shutting down! Closing socket...");
+      in.close();
+      out.close();
       clientSocket.close();
       input.close();
     } catch (Exception e) {
-      System.err.println("Error: failed to close socket.");
+      e.printStackTrace();
+      try {
+        clientSocket.close();
+      } catch(Exception ee) {
+        ee.printStackTrace();
+      }
+    } finally { 
       System.exit(1);
     }
     System.exit(0);
