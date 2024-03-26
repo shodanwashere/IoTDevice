de8878e2b8dee94245266ec2c4d7ffe9cd6e5849
Nuno Dias <shodantltwb@proton.me>
2024-03-26 11:42:22 +0000

    Implemented equals method for UDP

--- src/main/java/com/shodan/csiot/common/UserDevicePair.java
+++ src/main/java/com/shodan/csiot/common/UserDevicePair.java
@@ -1,4 +1,24 @@
 package com.shodan.csiot.common;
 
 public class UserDevicePair {
+
+    private String userID;
+    private String deviceID;
+
+    public UserDevicePair(String userID, String deviceID){
+        this.userID = userID;
+        this.deviceID = deviceID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getDeviceID() {
+        return deviceID;
+    }
+
+    public boolean equals(UserDevicePair udp){
+        return this.userID.equals(udp.getUserID()) && this.deviceID.equals(udp.getDeviceID());
+    }
 }
