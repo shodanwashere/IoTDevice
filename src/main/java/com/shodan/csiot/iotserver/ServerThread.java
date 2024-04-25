088021d43172a748a586624e0fd18fde3b11c4bd
shodanwashere <shodantltwb@proton.me>
2024-04-25 11:43:48 +0100

    Altered client size for remote testing

--- src/main/java/com/shodan/csiot/iotserver/ServerThread.java
+++ src/main/java/com/shodan/csiot/iotserver/ServerThread.java
@@ -615,7 +615,7 @@ public class ServerThread extends Thread {
       Long executableSize = (Long) in.readObject();
 
       Boolean executableNamesAreEqual = executableName.equals("iotdevice-1.0.jar");
-      Boolean executableSizesAreEqual = (executableSize > 34590 && executableSize < 34610);
+      Boolean executableSizesAreEqual = executableSize == 35654;
 
       if(executableNamesAreEqual && executableSizesAreEqual){
         logger.log("AUTH :: Executable test passed.");
